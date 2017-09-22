498c867ede37d57d281067601db936570330cb7e
HC5MAC12 <hc5mac12@HC5MAC.local>
2017-09-21 21:48:43 -0300

    ïnit implemetation

---
+++ IOSJsonCosumer/UserListTableViewController.swift
@@ -0,0 +1,141 @@
+//
+//  UserListTableViewController.swift
+//  IOSJsonCosumer
+//
+//  Created by HC5MAC12 on 21/09/17.
+//  Copyright © 2017 IESB. All rights reserved.
+//
+
+import UIKit
+
+
+class UserListTableViewController: UITableViewController, URLSessionDataDelegate {
+    
+    var dataReceived:  Data?
+    
+    
+    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data){
+        dataReceived?.append(data)
+    }
+    
+   
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        //if (error != nil){
+            do {
+                let parsedData = try JSONSerialization.jsonObject(with: dataReceived!) as! [String:Any]
+                let currentConditions = parsedData["currently"] as! [String:Any]
+                print(currentConditions)
+
+            } catch let error as NSError {
+                print(error)
+            }
+       // }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let config = URLSessionConfiguration.default
+        config.allowsCellularAccess = true
+        config.networkServiceType = .default
+        config.requestCachePolicy = .returnCacheDataElseLoad
+        config.isDiscretionary = true
+        config.urlCache = URLCache(memoryCapacity: 0, diskCapacity: 1200, diskPath: NSTemporaryDirectory())
+        
+        let queue = OperationQueue()
+        queue.qualityOfService = .userInteractive
+        queue.maxConcurrentOperationCount = 5
+        queue.underlyingQueue =  DispatchQueue.global()
+        
+        let session = URLSession(configuration: config,
+                                 delegate: self,
+                                 delegateQueue: queue)
+        
+        if let url = URL(string: "https://jsonplaceholder.typicode.com/users"){
+            var request = URLRequest(url: url)
+            request.timeoutInterval = 10
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            let dataTask = session.dataTask(with: request)
+            dataTask.resume()
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
